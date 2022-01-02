b9d0257f4354afc1aa507a5e92ff8b0628d849de
Udit Kumar Chatterjee <quantumudit@gmail.com>
2022-01-03 01:07:52 +0530

    basic querying with T-SQL

---
+++ DATABASES/DemoDB.sql
@@ -0,0 +1,107 @@
+﻿-- CREATING DATABASE --
+-- ***************** --
+
+USE master
+GO
+
+DROP DATABASE IF EXISTS DemoDB
+CREATE DATABASE DemoDB
+GO
+
+USE DemoDB
+GO
+
+-- CREATING TABLES --
+-- *************** --
+
+-- Creating 'person' table
+
+DROP TABLE IF EXISTS dbo.person
+CREATE TABLE dbo.person
+(
+	id int NOT NULL IDENTITY(100,1) PRIMARY KEY,
+	name nvarchar(50) NOT NULL,
+	age int,
+	gender_id int,
+	city nvarchar(50),
+	email nvarchar(50) NOT NULL
+)
+
+-- creating gender table
+
+DROP TABLE IF EXISTS dbo.gender
+CREATE TABLE dbo.gender
+(
+	gender_id int NOT NULL PRIMARY KEY,
+	gender nvarchar(50) NOT NULL,
+)
+
+-- creating 'characters' table
+
+DROP TABLE IF EXISTS dbo.characters
+CREATE TABLE dbo.characters
+(
+	id int NOT NULL IDENTITY(100,1) PRIMARY KEY,
+	name nvarchar(50) NOT NULL,
+	gender nvarchar(20) NOT NULL,
+	city nvarchar(50) NOT NULL,
+	salary int NOT NULL
+)
+
+-- ADDING TABLE CONSTRAINTS --
+-- ************************ --
+
+-- Adding foreign key constraints
+
+ALTER TABLE dbo.person ADD CONSTRAINT FK_person_gender_id
+FOREIGN KEY (gender_id) REFERENCES dbo.gender(gender_id)
+
+-- Adding default constraint to 'gender_id' column in 'persons' table
+
+ALTER TABLE dbo.person ADD CONSTRAINT DF_person_gender_id
+DEFAULT 3 FOR gender_id
+
+-- Adding check constraint to 'age' column in 'persons' table
+
+ALTER TABLE dbo.person ADD CONSTRAINT CK_person_age
+CHECK (age > 0 AND age < 150)
+
+-- Adding unique constraint on 'gender' column in 'gender' table
+
+ALTER TABLE dbo.gender ADD CONSTRAINT UQ_gender_gender
+UNIQUE(gender)
+
+-- INSERTING DATA INTO TABLES --
+-- ************************** --
+
+-- Inserting data into 'gender' table
+
+INSERT INTO dbo.gender(gender_id,gender) VALUES (1,'Male')
+INSERT INTO dbo.gender(gender_id,gender) VALUES (2,'Female')
+INSERT INTO dbo.gender(gender_id,gender) VALUES (3,'Others')
+
+-- Inserting data into 'person' table
+
+INSERT INTO dbo.person(name,age,gender_id,city,email) VALUES ('Rockie',58,2,'New York','r@t.com')
+INSERT INTO dbo.person(name,age,gender_id,city,email) VALUES ('Paul',25,3,'Chicago','schasmar1@artisteer.com')
+INSERT INTO dbo.person(name,age,gender_id,city,email) VALUES ('Henka',88,2,'Houston','hhardwicke2$japanpost.jp')
+INSERT INTO dbo.person(name,age,gender_id,city,email) VALUES ('Eolande',58,3,'Los Angeles','erapinett3@irs.gov')
+INSERT INTO dbo.person(name,age,gender_id,city,email) VALUES ('Weston',86,2,'Dallas','wjewke4$desdev.cn')
+INSERT INTO dbo.person(name,age,gender_id,city,email) VALUES ('Bernie',80,2,'Atlanta','bpauly5@ning.com')
+INSERT INTO dbo.person(name,age,gender_id,city,email) VALUES ('Craggy',70,1,'Los Angeles','c@g.fr')
+INSERT INTO dbo.person(name,age,gender_id,city,email) VALUES ('Giordano',65,2,'New York','gburgwyn7@spiegel.de')
+INSERT INTO dbo.person(name,age,gender_id,city,email) VALUES ('Regen',84,3,'Saint Paul','roppery8@diigo.com')
+INSERT INTO dbo.person(name,age,gender_id,city,email) VALUES ('Paul',41,2,'Chicago','pbeeson9@newsvine.com')
+
+-- Inserting data into 'characters' table
+
+INSERT INTO dbo.characters(name,gender,city,salary) VALUES ('Peter','Male','London',4688)
+INSERT INTO dbo.characters(name,gender,city,salary) VALUES ('Mary','Female','New York',3518)
+INSERT INTO dbo.characters(name,gender,city,salary) VALUES ('Gwen','Female','Sydney',5424)
+INSERT INTO dbo.characters(name,gender,city,salary) VALUES ('Ben','Male','London',3893)
+INSERT INTO dbo.characters(name,gender,city,salary) VALUES ('May','Female','New York',2283)
+INSERT INTO dbo.characters(name,gender,city,salary) VALUES ('Norman','Male','Sydney',6338)
+INSERT INTO dbo.characters(name,gender,city,salary) VALUES ('Harry','Male','Sydney',5154)
+INSERT INTO dbo.characters(name,gender,city,salary) VALUES ('Curt','Male','London',4763)
+INSERT INTO dbo.characters(name,gender,city,salary) VALUES ('Betty','Female','New York',5982)
+INSERT INTO dbo.characters(name,gender,city,salary) VALUES ('Lucy','Female','London',2395)
---
+++ readme.md
@@ -0,0 +1,87 @@
+![Project Logo][project_logo]
+
+---
+
+<h4 align="center">Learning T-SQL Querying with SQL Server and Azure Data Studio</a></h4>
+
+<p align='center'>
+<img src="https://i.ibb.co/KxfMMsP/built-with-love.png" alt="built-with-love" border="0">
+<img src="https://i.ibb.co/MBDK1Pk/powered-by-coffee.png" alt="powered-by-coffee" border="0">
+<img src="https://i.ibb.co/CtGqhQH/cc-nc-sa.png" alt="cc-nc-sa" border="0">
+</p>
+
+<p align="center">
+  <a href="#overview">Overview</a> •
+  <a href="#prerequisites">Prerequisites</a> •
+  <a href="#support">Support</a> •
+  <a href="#license">License</a>
+</p>
+
+## Overview
+
+This repository contains notebooks that can help in learning T-SQL queries from the very basics to advance level.
+
+The repository directory structure is as follows:
+
+Querying-with-TSQL<br>
+├─ DATABASES<br>
+├─ NOTEBOOKS<br>
+├─ RESOURCES<br>
+
+The type of content present in the directories is as follows:
+
+**DATABASES**
+
+This directory contains the T-SQL script of the databases used for all types of querying in this repository.
+
+You may execute the SQL scripts to create the databases in you SQL Server machine
+
+**NOTEBOOKS**
+
+This directory contains the SQL Notebooks that has the SQL queries along with descriptive notes.
+
+The SQL Notebooks are basically the Jupyter Notebooks with T-SQL Kernel and can be edited with Azure Data Studio.
+
+**RESOURCES**
+
+This directory contains images, icons, layouts, etc. that are used in this project
+
+## Prerequisites
+
+In order to follow along with this project one need to have basic understanding of the SQL Server & Azure Data Studio interface.
+
+## Support
+
+If you have any doubts, queries or, suggestions then, please connect with me in any of the following platforms:
+
+[![Linkedin Badge][linkedinbadge]][linkedin] [![Twitter Badge][twitterbadge]][twitter]
+
+If you like my work then, you may support me at Patreon:
+
+<a href="https://www.patreon.com/quantumudit" target="_blank">
+<img src="https://i.ibb.co/94bkJwp/become-a-patreon.png" alt="become-a-patreon" border="0" width="170" height="50">
+</a>
+
+## License
+
+<a href = 'https://creativecommons.org/licenses/by-nc-sa/4.0/' target="_blank">
+    <img src="https://i.ibb.co/mvmWGkm/by-nc-sa.png" alt="by-nc-sa" border="0" width="88" height="31">
+</a>
+
+This license allows reusers to distribute, remix, adapt, and build upon the material in any medium or format for noncommercial purposes only, and only so long as attribution is given to the creator. If you remix, adapt, or build upon the material, you must license the modified material under identical terms.
+
+<!-- Image Links -->
+
+[project_logo]: RESOURCES/project_cover_image.png
+
+<!-- External Links -->
+
+<!-- Profile Links -->
+
+[linkedin]: https://www.linkedin.com/in/uditkumarchatterjee/
+[twitter]: https://twitter.com/quantumudit
+
+<!-- Shields Profile Links -->
+
+[linkedinbadge]: https://img.shields.io/badge/-uditkumarchatterjee-0e76a8?style=flat&labelColor=0e76a8&logo=linkedin&logoColor=white
+[twitterbadge]: https://img.shields.io/badge/-@quantumudit-1ca0f1?style=flat&labelColor=1ca0f1&logo=twitter&logoColor=white&link=https://twitter.com/quantumudit
